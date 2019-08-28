99e2736564c283a6abf4f0d95697f4da6b95e887
LanceP51 <appleguy05@live.com>
2019-08-28 15:30:54 -0400

    fetched, looped and consoled list of objects. aug 28 330pm

--- src/scripts/metrogovAPI.js
+++ src/scripts/metrogovAPI.js
@@ -4,10 +4,12 @@
 //   "$$app_token": yourToken
 // })
 
+// pull from json server since api is down and looping through array to list all objects
 const parksPull = {
     getAllParks: ()=>{
         fetch("http://localhost:3000/parks")
     .then(parkLocations => parkLocations.json())
     .then(park=>{
-        park.forEach(park=>console.log(park))})
-    }}
+        park.forEach(park=>console.log(park))
+    })
+}}
