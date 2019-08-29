ea57f6774d867b70c52ad20fca8a790662a219fb
Daryl Adkins <daryl.adkins@gmail.com>
2019-08-29 15:41:59 -0400

    end of day 2, working MVP

--- src/scripts/eventListener.js
+++ src/scripts/eventListener.js
@@ -1,31 +1,14 @@
 //Function to grab user input value and send to eventbrite.js for searching API
 
-
+const meetUpsSearchButton = 
 document.querySelector(".meetUpsSearchButton").addEventListener("click", function() {   
         console.log("You clicked the meetups search button from the event listener js")  
-        const meetUpsValue = document.querySelector(".meetups").value
-        console.log(meetUpsValue)
-        
-  return
+        let meetUpsValue = document.querySelector(".meetups").value
+       eventAPIManager.getAllMeetups(meetUpsValue)
+       console.log(meetUpsValue)
   })
 
-//   const apiManagerBreweries ={
-//     getAllBreweries: () =>{
-//         const searchBarInput = document.querySelector("#breweryInput").value
-//         console.log(searchBarInput)
-//         return fetch(`https://api.openbrewerydb.org/breweries?by_state=tennessee&by_city=${searchBarInput}`)
-//         .then(response => response.json())
-//         .then(breweryToPrint=>{
-//             console.log(breweryToPrint)
-//             breweryToPrint.forEach(singleBrewery =>{
-//                 document.querySelector("#bigOlContainer").innerHTML += buildBreweryHtml.buildEntryCard(singleBrewery)
 
-//   <p id="outputMeetups"></p>
-
-//   <p class="input">
-//   <input type="text" class="meetups" placeholder="Search for Meetups">
-//   <button class="searchButton">Search</button>
-// </p>
 
 
 
--- src/scripts/printToDOM.js
+++ src/scripts/printToDOM.js
@@ -5,3 +5,11 @@ ${brewName.name}
 </p>
 <button id="brewSaveBTN">Save</button>
 `}
+
+
+//Print Meetups search results to the DOM
+function printMeetUpHTML (meetUps){
+    const printMeetUps= document.querySelector("#outputMeetups")
+    printMeetUps.innerHTML+=`<p>${meetUps.name.html}</p>
+    <button id="meetUpsSaveBTN">Save</button>
+    `}
