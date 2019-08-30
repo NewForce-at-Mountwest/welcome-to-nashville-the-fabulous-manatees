c878dc1a26c4e30b3aca8cf0cdf07ba6f291a815
8bitmel0dee <8bitmel0dee@gmail.com>
2019-08-30 11:11:36 -0400

    pull

--- src/scripts/printToDom.js
+++ src/scripts/printToDom.js
@@ -5,21 +5,3 @@ ${brewName.name}
 </p>
 <button id="brewSaveBTN">Save</button>
 `}
-
-
-// TicketMaster print results to screen
-
-function concertPrintToScreen(concertResult){
-
-    const printConcert = document.querySelector("#outputConcerts")
-    
-    printConcert.innerHTML+=
-    `
-    <p>
-    ${concertResult.name}<br>
-    ${concertResult.classifications[0].genre.name}
-    </p>
-    <button id="concertSaveBTN">Save</button>
-    `
-
-};
